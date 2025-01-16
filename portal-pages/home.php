e3e59124be9992eaa298fd2dbf199efd2e627c51
RaffiRmdhn <klsterbuka@gmail.com>
2025-01-16 22:00:30 +0700

    Template tailwind css

---
+++ portal-pages/home.php
@@ -0,0 +1,19 @@
+<?php include_once '../helper.php' ?>
+
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="tailwind-o.css" rel="stylesheet">
+</head>
+
+<body class="h-screen flex flex-col justify-center items-center">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <a href="<?= $BASE_URL ?>/admin-pages/daftar-mekanik.php" class="text-red-500 font-semibold">Daftar Mekanik</a>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./portal-pages/**/*.{html,js,php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
