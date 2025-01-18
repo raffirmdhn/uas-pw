30702325f87cb8ce89d41ee4af27f8e88d5fc6d7
Dower4you <mhmaddiqbll@gmail.com>
2025-01-18 21:23:42 +0700

    Merevisi daftar-mekanik.php dan Menambahkan edit-mekanik.php

--- admin-pages/daftar-mekanik.php
+++ admin-pages/daftar-mekanik.php
@@ -231,7 +231,7 @@
               <!-- Default Box -->
               <div class="card">
                 <div class="card-header">
-                  <h3 class="card-title">List Member Mekanik</h3>
+                  <h3 class="card-title">Detail Mekanik</h3>
                   <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                       <i class="fas fa-minus"></i>
